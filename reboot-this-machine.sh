7f299939df003c145e97b98e075752721f585b0f
Flynnt Knapp <bruce.stull@gmail.com>
2025-06-22 11:27:32 -0400

    Add 'reboot-this-machine.sh' reboot script.

---
+++ reboot-this-machine.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+#
+# Script: reboot-this-machine.sh
+# Description:
+#   Shutdown and immediately reboot the local machine.
+#
+# Usage:
+#   sudo ./reboot-this-machine.sh
+#
+# Configuration:
+#   None
+#
+
+set -euo pipefail
+
+# —— Reboot ——  
+sudo shutdown -r now
