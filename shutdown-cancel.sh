6589b0e77f7da41d7ebf3e56793520588f6b0c63
Flynnt Knapp <bruce.stull@gmail.com>
2025-06-22 21:46:13 -0400

    Add 'shutdown-cancel.sh' script.

---
+++ shutdown-cancel.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+#
+# Script: shutdown-cancel.sh
+# Description:
+#   Cancel previous shutdown order of this local machine.
+#
+# Usage:
+#   sudo ./shutdown-cancel.sh
+#
+# Configuration:
+#   None
+#
+
+set -euo pipefail
+
+# —— Shutdown in 1 minute ——  
+sudo shutdown -h +1
